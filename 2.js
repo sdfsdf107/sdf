73aa5a910367f7c6ccf4fe53981a5c4b4e761e06
Your Name <you@example.com>
2020-09-29 18:35:55 +0800

    2.js

---
+++ 2.js
@@ -0,0 +1,15 @@
+var a=10
+var b=5000
+var c = 550
+var d = 1
+
+console.log((2*a*c/d-Math.sqrt(4*a*a*c*c/(d*d) -(4*a*a*c*c/d/d-4*a*b*c/d)))/2/(c/d))
+console.log((2*a*c/d+Math.sqrt(4*a*a*c*c/(d*d) -(4*a*a*c*c/d/d-4*a*b*c/d)))/2/(c/d))
+
+ a=10
+ b=5000
+ c = 450
+ d = 1
+
+console.log((2*a*c/d-Math.sqrt(4*a*a*c*c/(d*d) -(4*a*a*c*c/d/d-4*a*b*c/d)))/2/(c/d))
+console.log((2*a*c/d+Math.sqrt(4*a*a*c*c/(d*d) -(4*a*a*c*c/d/d-4*a*b*c/d)))/2/(c/d))
